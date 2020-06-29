a0a19dd0c072b4aa49e0a3bc41121f86e4172553
Tiago Martins <TiagoPT@users.noreply.github.com>
2020-06-29 23:50:02 +0200

    Adds initial state of the project

---
+++ src/AspNetCoreTodo.Core/Services/Interfaces/ITodoItemService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+
+namespace AspNetCoreTodo.Core.Services
+{
+    public interface ITodoItemService
+    {
+        Task<TodoItem[]> GetIncompleteItemsAsync();
+    }
+}
---
+++ src/AspNetCoreTodo.Web/Controllers/TodoController.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using AspNetCoreTodo.Core.Services;
+using AspNetCoreTodo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreTodo.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            // Get to-do items from database
+            var items = await _todoItemService.GetIncompleteItemsAsync();
+
+            // Put items into a model
+            var viewModel = new TodoViewModel()
+            {
+                Items = items
+            };
+
+            // Pass the view to a model and render
+            return View(viewModel);
+        }
+    }
+}

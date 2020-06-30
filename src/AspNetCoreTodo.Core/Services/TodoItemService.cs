88449ed1b5784d42df091d8a668c5d5ce29e7302
Tiago Martins <TiagoPT@users.noreply.github.com>
2020-06-30 22:52:45 +0200

    Adds implementation of ITodoItemService based on access to database via EF

---
+++ src/AspNetCoreTodo.Core/Services/TodoItemService.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Data;
+using AspNetCoreTodo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreTodo.Core.Services
+{
+    public class TodoItemService : ITodoItemService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TodoItemService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TodoItem[]> GetIncompleteItemsAsync()
+        {
+            var items = await _context.Items
+                .Where(x => x.IsDone == false)
+                .ToArrayAsync();
+            return items;
+        }
+    }
+}

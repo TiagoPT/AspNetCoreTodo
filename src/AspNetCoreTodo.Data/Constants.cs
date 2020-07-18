9ee1fd58098278aab2b55ba4f503c44579cf9f0a
Tiago Martins <TiagoPT@users.noreply.github.com>
2020-07-18 15:36:37 +0200

    Adds string constants

---
+++ src/AspNetCoreTodo.Data/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace AspNetCoreTodo.Data
+{
+    public static class Constants
+    {
+        public const string AdministratorRole = "Administrator";
+
+        public const string AdminstratorEmailConfigKey = "Accounts:AdminEmail";
+        public const string AdminstratorPasswordConfigKey = "Accounts:AdminPassword";
+        public const string TestUserEmailConfigKey = "Accounts:TestUserEmail";
+        public const string TestUserPasswordConfigKey = "Accounts:TestUserPassword";
+    }
+}

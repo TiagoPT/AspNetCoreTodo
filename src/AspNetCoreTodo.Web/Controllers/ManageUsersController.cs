a3c396c44d5d30401dc8919c33297534c7ba118f
Tiago Martins <TiagoPT@users.noreply.github.com>
2020-07-18 15:42:34 +0200

    Adds page to see list of users, and manage them

---
+++ src/AspNetCoreTodo.Web/Controllers/ManageUsersController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Data;
+using AspNetCoreTodo.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreTodo.Web.Controllers
+{
+    [Authorize(Roles = Constants.AdministratorRole)]
+    public class ManageUsersController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ManageUsersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var admins = await _userManager.GetUsersInRoleAsync(Constants.AdministratorRole);
+            var allUsers = await _userManager.Users.ToListAsync();
+
+            var model = new ManageUsersViewModel
+            {
+                Administrators = admins,
+                AllUsers = allUsers
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ src/AspNetCoreTodo.Web/Models/ManageUsersViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using AspNetCoreTodo.Data;
+
+namespace AspNetCoreTodo.Web.Models
+{
+    public class ManageUsersViewModel
+    {
+        public IEnumerable<ApplicationUser> Administrators { get; internal set; }
+        public IEnumerable<ApplicationUser> AllUsers { get; internal set; }
+    }
+}

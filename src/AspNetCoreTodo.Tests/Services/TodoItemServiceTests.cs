d0c24457bae2a1abb3d0a27c785d5c7936b01125
Tiago Martins <TiagoPT@users.noreply.github.com>
2020-07-19 21:32:25 +0200

    Adds unit testing with xUnit

---
+++ src/AspNetCoreTodo.Tests/Services/TodoItemServiceTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Core.Services;
+using AspNetCoreTodo.Data;
+using AspNetCoreTodo.Models;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace AspNetCoreTodo.Tests.Services
+{
+    public class TodoItemServiceTests
+    {
+        [Fact]
+        public async Task AddItemAsyncShouldBeIncompleteAsync()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                            .UseInMemoryDatabase(databaseName: "Test_AddNewItem").Options;
+
+            // Set up a context (connection to the "DB") for writing
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new TodoItemService(context);
+
+                var fakeUser = new ApplicationUser
+                {
+                    Id = "fake-000",
+                    UserName = "fake@example.com"
+                };
+
+                await service.AddItemAsync(new TodoItem
+                {
+                    Title = "Testing?"
+                }, fakeUser.Id);
+            }
+
+            // Use a separate context to read data back from the "DB"
+            using (var context = new ApplicationDbContext(options))
+            {
+                var itemsInDatabase = await context
+                    .Items.CountAsync();
+                Assert.Equal(1, itemsInDatabase);
+
+                var item = await context.Items.FirstAsync();
+                Assert.Equal("Testing?", item.Title);
+                Assert.False(item.IsDone);
+            }
+        }
+
+        [Fact]
+        public async Task AddItemAsyncShouldBeDueIn3DaysAsync()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                            .UseInMemoryDatabase(databaseName: "Test_AddNewItem").Options;
+
+            // Set up a context (connection to the "DB") for writing
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new TodoItemService(context);
+
+                var fakeUser = new ApplicationUser
+                {
+                    Id = "fake-000",
+                    UserName = "fake@example.com"
+                };
+
+                await service.AddItemAsync(new TodoItem
+                {
+                    Title = "Testing?"
+                }, fakeUser.Id);
+            }
+
+            // Use a separate context to read data back from the "DB"
+            using (var context = new ApplicationDbContext(options))
+            {
+                var itemsInDatabase = await context
+                    .Items.CountAsync();
+                Assert.Equal(1, itemsInDatabase);
+
+                var item = await context.Items.FirstAsync();
+                Assert.Equal("Testing?", item.Title);
+
+                // Item should be due 3 days from now (give or take a second)
+                var difference = DateTimeOffset.Now.AddDays(3) - item.DueAt;
+                Assert.True(difference < TimeSpan.FromSeconds(1));
+            }
+        }
+    }
+}
